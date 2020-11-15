09072be31bf87d0cffdfd55915331994709a8f5e
Craig A. McDonald <craig@mcdonald.codes>
2020-11-15 22:06:06 +0000

    Maven - first build

---
+++ src/main/java/codes/mcdonald/craig/bankofboot/BankOfBootApplication.java
@@ -0,0 +1,13 @@
+package codes.mcdonald.craig.bankofboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOfBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codes/mcdonald/craig/bankofboot/BankOfBootApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.mcdonald.craig.bankofboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOfBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
